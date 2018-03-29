7a9f6aa7bdd556b69510896ac310ce85fbd2f297
Keishi Hattori <keishi.hattori@gmail.com>
2018-03-29 18:11:27 +0900

    fix

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ WP_PORT=8081
 JP_PORT=8888
 DIR="$(pwd)/home/$ID"
 mkdir $DIR
-sudo chown 1000 $DIR
+cp -r ../wpsearch $DIR/wpsearch
+sudo chown -R 1000:100 $DIR
 sudo docker run -it --rm -m 30G -p $WP_PORT:8081 -p $JP_PORT:8888 -v $DIR:/home/jovyan wpsearch-img
