3ef6f4a7f50f20252365a94a229a616f23de93e0
Keishi Hattori <keishi.hattori@gmail.com>
2018-03-29 18:14:53 +0900

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ ID=iguana
 WP_PORT=8081
 JP_PORT=8888
 DIR="$(pwd)/home/$ID"
-if [ -d "$DIRECTORY" ]; then
+if [ ! -d "$DIRECTORY" ]; then
     mkdir $DIR
     cp -r ../wpsearch $DIR/wpsearch
     sudo chown -R 1000:100 $DIR
