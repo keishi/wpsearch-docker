ec793ab800ac4cb1021f3848c52e7fcc1e544232
Keishi Hattori <keishi.hattori@gmail.com>
2018-03-30 07:23:19 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,10 +1,21 @@
-ID=iguana
-WP_PORT=8081
-JP_PORT=8888
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 3 ] || die "3 arguments required, $# provided"
+echo $2 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $2 provided"
+echo $3 | grep -E -q '^[0-9]+$' || die "Numeric argument required, $3 provided"
+
+ID=$1
+WP_PORT=$2
+JP_PORT=$3
+
 DIR="$(pwd)/home/$ID"
 if [ ! -d "$DIRECTORY" ]; then
-    mkdir $DIR
-    cp -r ../wpsearch $DIR/wpsearch
+    mkdir -p $DIR/work
+    cp -r ../wpsearch $DIR/work/wpsearch
     sudo chown -R 1000:100 $DIR
 fi
-sudo docker run -it --rm -m 30G -p $WP_PORT:8081 -p $JP_PORT:8888 -v $DIR:/home/jovyan wpsearch-img
+
+sudo docker run -it --rm -m 30G -e WPSEARCH_PORT=$WP_PORT -p $WP_PORT:8081 -p $JP_PORT:8888 -v $DIR:/home/jovyan wpsearch-img
